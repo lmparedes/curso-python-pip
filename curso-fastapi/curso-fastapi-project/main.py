88dc7f69dc818727e1275b4aaea28f12fc327fd4
Liliana <lmguerrero1996@gmail.com>
2026-03-06 17:06:19 -0300

    add main.py, models.py, requirements.py, db.py

--- curso-fastapi/curso-fastapi-project/main.py
+++ curso-fastapi/curso-fastapi-project/main.py
@@ -1,13 +1,74 @@
-from fastapi import FastAPI
-import datetime
+import zoneinfo
+from datetime import datetime
+from fastapi import FastAPI, HTTPException
+
+from models import Costumer, CostumerCreate, Transaction, Invoice
+from db import SessionDep
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello, Liliana!"}
+country_timezones = {
+    "CO": "America/Bogota",
+    "MX": "America/Mexico_City",
+    "AR": "America/Argentina/Buenos_Aires",
+    "BR": "America/Sao_Paulo",
+    "PE": "America/Lima",
+}
 
-@app.get("/hora")
-async def obtener_hora():
-    ahora = datetime.datetime.now()
-    return {"hora": ahora.strftime("%H:%M:%S")}
+@app.get("/time/{iso_code}")
+async def time(iso_code: str, format: str = "24"):
+    iso = iso_code.upper()
+    timezone_str = country_timezones.get(iso)
+
+    if not timezone_str:
+        raise HTTPException(status_code=404, detail="País no soportado")
+
+    tz = zoneinfo.ZoneInfo(timezone_str)
+    now = datetime.now(tz)
+
+    if format == "12":
+        time_str = now.strftime("%I:%M:%S %p")
+    else:
+        time_str = now.strftime("%H:%M:%S")
+
+    return {
+        "country": iso,
+        "timezone": timezone_str,
+        "time": time_str,
+        "format": format
+    }
+
+db_costumers: list[Costumer] = []
+
+#Crea un nuevo costumer y lo agrega a la base de datos simulada, luego devuelve el costumer creado
+@app.post("/costumers", response_model=Costumer)
+async def create_costumer(costumer_data: CostumerCreate, session: SessionDep):
+    costumer = Costumer.model_validate(costumer_data.model_dump())
+    #Asumiendo que se hace en la base de datos
+    costumer.id = len(db_costumers)
+    db_costumers.append(costumer)
+    return costumer
+
+#devuelve el costumer pasando el id del costumer, si no lo encuentra devuelve un error 404
+@app.get("/costumers/{costumer_id}", response_model=Costumer)
+async def get_costumer(costumer_id: int):
+    for costumer in db_costumers:
+        if costumer.id == costumer_id:
+            return costumer
+    
+    raise HTTPException(status_code=404, detail="Costumer not found")
+
+#devuelve la lista de costumers registrados en la base de datos simulada
+@app.get("/costumers", response_model=list[Costumer])
+async def list_costumer():
+    return db_costumers
+
+@app.post("/Transactions")
+async def create_transaction(transaction_data: Transaction):
+    # Aquí podrías agregar lógica para guardar la transacción en una base de datos
+    return transaction_data
+
+@app.post("/invoices")
+async def create_invoice(invoice_data: Invoice):
+    # Aquí podrías agregar lógica para guardar el cliente en una base de datos
+    return invoice_data
---
+++ curso-fastapi/curso-fastapi-project/models.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel
+from sqlmodel import SQLModel
+
+class CostumerBase(SQLModel):
+    name: str
+    description: str | None
+    email: str
+    age: int
+
+class CostumerCreate(CostumerBase):
+    pass
+
+class Costumer(CostumerBase, table=True):
+    id: int | None = None
+    
+
+class Transaction(BaseModel):
+    id: int
+    ammount: int
+    description: str
+
+class Invoice(BaseModel):
+    id: int
+    costumer: Costumer
+    transactions: list[Transaction]
+    total: int
+    
+    @property
+    def ammount_total(self):
+        return sum(transaction.ammount for transaction in self.transactions)
